49123b96c276f07d5dbf47cbbe9e2032b22b890b
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-11 20:20:25 -0300

    Add files via upload

---
+++ mina.cpp
@@ -0,0 +1,44 @@
+include "jugada.h"
+
+using namespace std;
+
+	void jugada::asignarOpcion(char opcionDeUsuario){
+	    
+		this->opcion = opcionDeUsuario;
+	
+	}
+	
+	void jugada::asignarFila(int filaDeUsuario){
+	    
+		this->fila = filaDeUsuario;
+	
+	}
+	
+	void jugada::asignarColumna(int columnaDeUsuario){
+	    
+		this->columna = columnaDeUsuario;
+	
+	}
+	
+	int jugada::realizarJugada(){
+		
+		int puntos=0;
+		
+		if(this->opcion=='m' || this->opcion=='M'){
+			
+			marcador();
+			
+			puntos = marcador.marcar(this->fila , this ->columna);
+			
+		}else if(this->opcion == 'd' || this->opcion == 'D'){
+			
+			destapador();
+			
+			destapador.destapar(this->fila , this->columna);
+		      
+		}else{
+			
+			arbitro.eliminarJugador();
+		}
+		return puntos ;
+	}
---
+++ mina.h
@@ -0,0 +1,46 @@
+#ifndef JUGADA_H_
+#define JUGADA_H_
+
+include <iostream>
+include "destapador.h"
+include "marcador.h"
+
+class jugada(){
+	
+	private:
+		
+		//atributos
+		char opcion;
+		int fila;
+		int columna;
+		int puntaje;
+		
+	public:
+
+		//metodos
+		
+		//pre: se pasa como parametro la opcion que el usuario ingreso, es decir una D si
+		//quiere destapar una casilla, M si la quiere marcar o R si quiere retirarse.
+		//post: queda la opcion de la jugada registrada con el dato recibido.
+		void asignarOpcion(char opcionDeUsuario);
+		
+		//pre: se pasa por parametro la fila que el usuario ingreso, esta debe ser valida,
+		// es decir dentro de las dimensiones especificadas a un comienzo del juego.
+		//post: la fila queda registrada con el dato recibido.
+		void asignarFila(int fila);
+		
+		//pre: se pasa por parametro la columna que el usuario ingreso, esta debe ser valida,
+		// es decir dentro de las dimensiones especificadas a un comienzo del juego.
+		//post: la columna queda registrada con el dato recibido.
+		void asignarColumna(int columna);
+		
+		//pre: 
+		//post: segun la opcion que se registro anteriormente se realizara la jugada, es
+		//decir, se destapara la casilla, se marcara esta, o se retirara el jugador, se 
+		//devolvera el puntaje ganado con la jugada hecha.
+		int realizarJugada();
+		
+		
+		
+}
+#endif
