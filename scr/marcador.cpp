497f179b42ba07021d230bfdcb4b13c919b00465
EddyVegaGarcia <32282833+EddyVegaGarcia@users.noreply.github.com>
2017-10-12 17:19:07 -0300

    Create marcador.cpp

---
+++ scr/marcador.cpp
@@ -0,0 +1,55 @@
+
+
+#include "marcador.h"
+
+using namespace std;
+
+//constructor
+Marcador::Marcador()
+{
+
+  crearclases();
+  filaJugada -> jugada.obtenerfilaJugada(); //Falta crear el obtener de Jugada
+  columnaJugada -> jugada.obtenerColumnaJugada(); //Falta
+  puntaje = 0;
+  
+ }
+ 
+//Creo punteros a las clases (sugerencias)
+void Marcador::crearclases()
+{
+
+  Jugada jugada;
+  Mapa mapa;
+  Tablero tablero;
+  
+}
+
+//Separado por metodos para mejor claridad
+
+/no es necesario la fila y columnas porque ya lo obtube em el contructor
+void Marcador::marcar() 
+{
+  
+  char bandera = B;
+  
+  if(!tablero.estaRevelado(this->filaJugada, this->columnaJugada))
+  {
+    tablero.asignarValorCasilla(this->filaJugada, this->columnaJugada, bandera);
+    puntaje (....)
+  }
+  else(tablero.obtenerCasilla(this->filaJugada, this->columnaJugada) == bandera)
+  {
+    //constructor copia tablero
+    tablero.tablero(this->filaJugada, this->columnaJugada);
+    puntaje(....)
+  }
+   
+}
+
+int Destapar::obtenerPuntajePorMarcar()
+{
+
+  return puntaje;
+
+}
