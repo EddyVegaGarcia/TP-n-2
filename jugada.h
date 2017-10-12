bd781457382c58a36515036de6527da49fef5388
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-11 21:24:39 -0300

    Update jugada.h

--- jugada.h
+++ jugada.h
@@ -24,16 +24,11 @@ class jugada(){
 		//post: queda la opcion de la jugada registrada con el dato recibido.
 		void asignarOpcion(char opcionDeUsuario);
 		
-		//pre: se pasa por parametro la fila que el usuario ingreso, esta debe ser valida,
+		//pre: se pasa por parametro la fila y la columna que el usuario ingreso, esta debe ser valida,
 		// es decir dentro de las dimensiones especificadas a un comienzo del juego.
-		//post: la fila queda registrada con el dato recibido.
-		void asignarFila(int fila);
-		
-		//pre: se pasa por parametro la columna que el usuario ingreso, esta debe ser valida,
-		// es decir dentro de las dimensiones especificadas a un comienzo del juego.
-		//post: la columna queda registrada con el dato recibido.
-		void asignarColumna(int columna);
-		
+		//post: la fila y la columna quedan registradas con los datos recibidos.
+		jugada(int fila, int columna);
+	
 		//pre: 
 		//post: segun la opcion que se registro anteriormente se realizara la jugada, es
 		//decir, se destapara la casilla, se marcara esta, o se retirara el jugador, se 
