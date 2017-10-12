7f4fb8e7a2b541759fe29178ddbf3e9e8ad8bd6f
EddyVegaGarcia <32282833+EddyVegaGarcia@users.noreply.github.com>
2017-10-12 05:29:03 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,87 @@
-#include <iostream>
-#include "buscaminas.h"
+#ifndef MARCADOR_H_
+#define MARCADOR_H_
 
-int main()
-{
+#include "jugada.h"
+#include "matriz.h"
 
 
+class Destapador{
+	
+	private:
+		//Atributos
+		int puntaje;
+		int ubicacion;
+	
+	public:
+		//Metodos
+		
+		/*
+		 *Pre:
+		 *
+		 * Post: Se asigna la ubicacion que se desea marcar y se inicializa puntaje en 0.
+		 */
+		Marcador ();
+		
+		/*
+		 *Pre: NO MARCAR DONDE YA ESXISTE UN MARCADOR.
+		 *
+		 * Post: Marca la casilla con la ubicacion que se jugó.
+		 */
+		void marcar();
+		
 
-  
-  return 0;
-}
+		
+		/*
+		 *Pre: Se recibe por parametro un puntero a la matriz que contiene al tablero.
+		 *
+		 * Post: Colaca la marca donde se supone que hay una mina., no se revela y por 
+		 * consecuencia no se pierde.
+		 */	
+		void marcarTablero(matriz* matrizTablero);
+			
+		
+		/*
+		 *Pre: Se pasa por parametro un valor entero valido.
+		 *
+		 * Post: Segun las reglas de juego se le asigna puntos segun si acertó con el marcador o no.
+		 */
+		void asignarPuntaje(int punt);
+		
+		/*
+		 *Pre: 
+		 *
+		 * Post: Devuelve el puntaje que obtuvo el jugador.
+		 */
+		int obtenerPuntaje();
+		
+		
+	private:
+		//metodos privados.
+		
+		
+		/*
+		 *Pre:
+		 *
+		 * Post: Asigna la ubicacion de la casilla obtenida de jugada.
+		 */
+		int asiganrUbicacion();
+		
+		/*
+		 *Pre: Se pasa por parametro un puntero a la matriz que contiene el tablero.
+		 *
+		 * Post: Cofirma que la cailla toma uno de los tres tipos.
+		 */
+		char verificarTablero(matriz* matrizTablero);
+		
+		
+		/*
+		 *Pre: Se pasa por parametro un puntero a la matriz que contiene el tablero.
+		 *
+		 * Post: Cofirma que la cailla toma uno de los tres tipos.
+		 */
+		char verificarMapa(matriz* matrizMapa);
+				
+		
+};
+
+#endif /*MARCADOR_H_*/
