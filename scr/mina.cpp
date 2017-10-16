78d7e9b270a9ed5e0d79e292246b99634de75409
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-16 15:25:20 -0300

    Create mina.cpp

---
+++ scr/mina.cpp
@@ -0,0 +1,33 @@
+# include "mina.h"
+
+void Mina::asignarFila(uint fila){
+  this->fila=fila;
+}
+void Mina::asignarColumna(uint columna){
+  this->columna=columna;
+}
+
+Mina::Mina(uint fila, uint columna){
+  asignarFila(fila);
+  asignarColumna(columna);
+}
+
+uint Mina::obtenerFila(){
+  return fila;
+}
+
+uint Mina::obtenerColumna(){
+  return columna;
+}
+
+bool Mina::estaJuntoAMina(uint filaAComparar, uint columnaAComparar){
+  bool estaAlrededor=false;
+  for(uint i=fila-1; i<fila+2;; i++){
+    for(uint j=columna-1;j<columna+2;j++){
+      if (i==filaAcomparar && j==columnaAComparar)
+        estaAlrededor=true;
+    }
+  }
+  
+  retun estaAlrededor;
+}
