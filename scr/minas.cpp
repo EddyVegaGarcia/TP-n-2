3d7b307f178c5ba1efb692a1c2bb5318d8d98b2d
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-16 19:58:34 -0300

    Update minas.cpp

--- scr/minas.cpp
+++ scr/minas.cpp
@@ -1,7 +1,26 @@
-include "minas.h"
+#include "minas.h"
+#include "minero.h"
+const char MINA 'X';
 
 
-Minas::Minas(uint cantidadMinas){
-    Minero minero(cantidadMinas);
-    minero.
+Minas::Minas(uint dimFilas, uint dimColumnas, uint dificultad){
+    
+    Minero minero(dimFilas , dimColumnas , dificultad);
+    Mina* puntero = minero.sembrarMinas();
+    this->minasAlmacenadas = puntero;
+}
+char Minas::calcularValorDeCasilla(uint filaCasilla, uint columnaCasilla){
+    char valor = '0';
+    minasAlmacenadas->iniciarCursor();
+    do{
+       Mina minaActual = minasAlmacenadas->ObtenerCursor();
+       if(minaActual.estaJuntoAMina(filaCasilla , columnaCasilla)){
+           valor ++;
+       }
+    }
+    while(minasAlmacenadas->avanzarCursor() &&  !minaActual.esMina(filaCasilla, columnaCasilla))
+    if (minaActual.esMina(filaCasilla, columnaCasilla)){
+        valor = MINA;
+    }  
+    return valor;
 }
