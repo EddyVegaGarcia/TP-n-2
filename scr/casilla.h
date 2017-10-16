a8fdeb55b2c95bf774442f8cae9accb88c3729d5
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-16 15:38:09 -0300

    Create casilla.h

---
+++ scr/casilla.h
@@ -0,0 +1,28 @@
+typedef unsigned int uint;
+
+class Casilla{
+private:
+  uint fila;
+  uint columna;
+  char valor;
+  
+public:
+
+ /*Pre: recibe fila y columna dentro del rango indicado por el jugador
+* Post: crea un objeto casilla, con la fila y columna recibidas. */
+  Casilla(uint fila, uint columna);
+  
+  /* Post: devuelve la fila*/
+  uint obtenerFila();
+  
+  /* Post: devuelve la fila*/
+  uint obtenerColumna();
+  
+  /* Post: devuelve el valor */
+  char obtenerValor();
+
+  void asignarFila();
+  void asignarColumna();
+  void asignarValor();
+
+};
