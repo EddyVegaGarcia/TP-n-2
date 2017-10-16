8ba55037e201573b36f5c972763d367c2e31e2a6
Tubio <32169593+Tubio@users.noreply.github.com>
2017-10-16 01:43:36 -0300

    Update arbitro.cpp

--- scr/arbitro.cpp
+++ scr/arbitro.cpp
@@ -1,2 +1,47 @@
 // tengo una duda con cursores y con como vamos a usar el objeto
 // lo vemos despues 
+
+#include "arbitro.h"
+#include "lista.h"
+
+
+void Arbitro::Arbitro(int cantidadJugadores){
+
+	char caracterDeJugador = 'A';
+
+	eliminados = NULL;
+
+	for(int i=1; i<=cantidadJugadores; i++){
+
+		jugadores.insertarJugador(i,caracterDeJugador);
+		caracterDeJugador++;
+
+	}
+
+	jugadorActual = jugadores.primero;
+
+}
+
+void Arbitro::avanzarJugador(){
+
+	jugadorActual = jugadorActual.siguiente;
+
+}
+
+bool Arbitro::haGanado(Jugador aVerificar){
+
+	bool jugadorGano = false;
+
+}
+
+bool Arbitro::mostrarJugador(){
+
+	std::cout<<"Jugador: "<<*jugadorActual.alias<<std::endl;
+
+}
+
+bool Arbitro::mostrarPuntaje(){
+
+	std::cout<<"Puntaje: "<<*jugadorActual.puntaje<<std::endl;
+
+}
