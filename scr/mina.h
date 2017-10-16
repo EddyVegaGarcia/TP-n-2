631c86cb494ff48559f6a3a24ad4559ee1a72897
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-16 14:56:32 -0300

    Create mina.h

---
+++ scr/mina.h
@@ -0,0 +1,25 @@
+typedef unsigned int uint;
+
+class Mina{
+private:
+  uint fila;
+  uint columna;
+  
+  void asignarFila(uint fila);
+  void asignarColumna(uint columna);
+
+
+public:
+/*Pre: recibe fila y columna dentro del rango indicado por el jugador
+* Post: crea un objeto mina, con la fila y columna recibidas. */
+  Mina(uint fila, uint columna);  
+  
+  /* Post: devuelve la fila*/
+  uint obtenerFila();
+  
+  /* Post: devuelve la fila*/
+  uint obtenerColumna();
+  
+  // algún método para ver los alrededores, 'o algo así' ^_^
+
+}
