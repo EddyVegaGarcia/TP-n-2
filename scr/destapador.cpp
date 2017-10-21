4e3ffb5fcafbdb8edb8c9e174c09da0e30f3fe58
Tubio <32169593+Tubio@users.noreply.github.com>
2017-10-21 16:23:08 -0300

    Update destapador.cpp

--- scr/destapador.cpp
+++ scr/destapador.cpp
@@ -64,3 +64,32 @@ int Destapar::obtenerPuntajePorRevelar()
   return puntaje;
 
 }
+
+void epidemia(char tablaMinada[][ancho],char tablaBloqueada[][ancho],int fila, int columna,int &contadorCeldas){
+	//si hay un cero, desbloquea los alrededores hasta encontrarse con un numero distinto de cero (recursiva)
+  
+  //tablaMinada es la matriz completa y tablaBloqueada es la matriz que interactua con el usuario, la funcion
+  //recibe ambas y luego una posicion representada por Fila Columna, chequea los alrededores de fila columna
+  //contadorCeldas no se usa en este trabajo CREO.
+
+	if(tablaBloqueada[fila-1][columna-1]=='*' || tablaBloqueada[fila-1][columna-1]=='B'){
+		tablaBloqueada[fila-1][columna-1]=tablaMinada[fila-1][columna-1];
+		--contadorCeldas;
+
+		if(tablaMinada[fila-1][columna-1]=='0'){
+
+			if(fila-1 !=0 &&tablaMinada[fila-2][columna-1]!='x' && tablaBloqueada[fila-2][columna-1]=='*')
+				expandir(tablaMinada,tablaBloqueada,fila-1,columna,contadorCeldas);
+
+			if(columna-1 !=0 &&tablaMinada[fila-1][columna-2]!='x' && tablaBloqueada[fila-1][columna-2]=='*')
+				expandir(tablaMinada,tablaBloqueada,fila,columna-1,contadorCeldas);
+
+			if(columna !=ancho && tablaMinada[fila-1][columna]!='x' && tablaBloqueada[fila-1][columna]=='*')
+				expandir(tablaMinada,tablaBloqueada,fila,columna+1,contadorCeldas);
+
+			if(fila !=largo && tablaMinada[fila][columna-1]!='x' && tablaBloqueada[fila][columna-1]=='*')
+				expandir(tablaMinada,tablaBloqueada,fila+1,columna,contadorCeldas);
+
+		}
+	}
+}
