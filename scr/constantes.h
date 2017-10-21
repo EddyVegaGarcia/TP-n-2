5a34b20bf2cee374764efc892129bb61148628c6
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-21 19:30:14 -0300

    Update constantes.h

--- scr/constantes.h
+++ scr/constantes.h
@@ -5,8 +5,10 @@ typedef unsigned int uint;
 const char VACIO = '0';
 const char MINA = 'X';
 const char BANDERA = 'B';
+
 const int PERDIO_PARTIDA = 3;
 const int SE_RETIRO = 4;
+const int SIGUE_JUGANDO = 0;
 
 // pongan sus constanteessssss :)
 
