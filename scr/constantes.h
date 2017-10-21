23bb81a40387cf41b2c744aba68d6bcb73efd146
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-21 19:20:26 -0300

    Update constantes.h

--- scr/constantes.h
+++ scr/constantes.h
@@ -1,9 +1,12 @@
 
 typedef unsigned int uint;
 
+
 const char VACIO = '0';
 const char MINA = 'X';
+const char BANDERA = 'B';
 const int PERDIO_PARTIDA = 3;
+const int SE_RETIRO = 4;
 
 // pongan sus constanteessssss :)
 
