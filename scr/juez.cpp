16aeb662e8e70dde4967fba09dbd98b75ad882e7
Tubio <32169593+Tubio@users.noreply.github.com>
2017-10-26 00:51:18 -0300

    Create juez.cpp

---
+++ scr/juez.cpp
@@ -0,0 +1,51 @@
+
+
+
+#include "juez.h"
+
+	Juez::Juez(unsigned int cantidadJugadores,Mapa* recibido){
+
+		tableroDeJuego = recibido;
+		Lista<jugador*> ingresantes();
+		this->jugadores = ingresantes;
+
+		char alias = 'A';
+
+		for (int i=0; i<cantidadJugadores;i++){
+
+			Jugador* entrante = new Jugador(alias+i,recibido);
+			insertarJugador(entrante);
+		}
+
+		this->jugadores->iniciarCursor();
+
+	}
+
+	void Juez::avanzarJugador(){
+
+		Jugador* siguiente;
+		bool puedeJugar=false;
+
+		do{
+			this->jugadores->avanzarCursor();
+			Jugador* siguiente = this->jugadores->obtenerCursor();
+
+			if(siguiente->obtenerEstado() == 0)
+				puedeJugar = true;
+
+		}while(!puedeJugar);
+
+	}
+
+	void Juez::mostrarJugador(){
+
+		Jugador* elegido;
+		elegido = this->jugadores->obtenerCursor();
+
+		elegido->mostrar();
+	}
+
+	void Juez::insertarJugador(Jugador* nuevo){
+
+		this->jugadores->insertar(nuevo);
+	}
