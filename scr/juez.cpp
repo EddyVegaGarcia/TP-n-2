4dd61f25cf87b5e6d2c026655b1960269fc692b0
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-28 17:06:23 -0300

    Update juez.cpp

--- scr/juez.cpp
+++ scr/juez.cpp
@@ -4,7 +4,7 @@
 #include "juez.h"
 
 	Juez::Juez(unsigned int cantidadJugadores,Mapa* mapaRecibido){
-
+		cantidadDeJugadores = cantidadJugadores;
 		tableroDeJuego = mapaRecibido;
 		Lista<Jugador*>* ingresantes;
 		this->jugadores = ingresantes;
@@ -50,14 +50,47 @@
 		this->jugadores->insertar(nuevo);
 	}
 
-	Juez::inicializarJuego();
+	void Juez::inicializarJuego(){
+		int jugadoresQuePerdieron = 0 ;
+		
+		this->jugadores->inicializarCursor();
+		
+		jugador* jugadorActual;
+		
+		while(jugadoresQuePerdieron<cantidadDeJugadores){
+			
+			jugadorActual = this->jugadores->obtenerCursor();
+			
+			if(jugadorActual->obtenerEstado() == SIGUE_JUGANDO){
+				
+				sigueJugando(jugadorActual, jugadoresQuePerdieron, tableroDeJuego);
+			
+			}
+			jugadores->avanzarCursor();
+		}
+	
+	
+	}
+ 	void sigueJugando(jugador* jugadorActual, int &jugadoresQuePerdieron, mapa* tableroDeJuego){
+	
+		jugadorActual->iniciarJugada();
+				
+			if(jugadorActual->obtenerEstado() == PERDIO_PARTIDA){
+					
+				  jugadoresQuePerdieron++;
+				  
+				   jugadorActual->asignarEstado(NO_ESTA_JUGANDO);
+			}
+		 tableroDeJuego->mostrarMapa;
+	}
+
        Juez::~juez(){
 	       
        		while( !(this->jugadores->estaVacia()) ){
 			jugador* jugadorActual;
 			char alias;
 			int puntaje;
-	   		this->jugadores->iniciarCursor();
+	   		this->jugadores->inicializarCursor;
 			jugadorActual = this->jugadores->obtenerCursor();
 			alias = jugardorActual->obtenerAlias();
 			puntaje = jugadorActual->obtenerPuntaje();
