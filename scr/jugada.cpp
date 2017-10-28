5076aeb03aaa8b0d4707e0b1cf2d009c46469a61
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-28 20:09:49 -0300

    Update jugada.cpp

--- scr/jugada.cpp
+++ scr/jugada.cpp
@@ -3,50 +3,50 @@ include "constantes.h"
 
 using namespace std;
         
-	jugada::jugada(mapa* mapaActual){
+	Jugada::Jugada(Mapa* mapaActual){
 		this->mapa = mapaActual;
 		this->fila = 0;
 		this->columna = 0;
 		this->puntaje = 0;
 	}
 
-	void jugada::asignarOpcion(char opcionDeUsuario){
+	void Jugada::asignarOpcion(char opcionDeUsuario){
 	    
 		this->opcion = opcionDeUsuario;
 	
 	}
 
-	void jugada::asignarFila(int filaDeUsuario){
+	void Jugada::asignarFila(int filaDeUsuario){
 	    
 		this->fila = filaDeUsuario;
 	
 	}
 	
-	void jugada::asignarColumna(int columnaDeUsuario){
+	void Jugada::asignarColumna(int columnaDeUsuario){
 	    
 		this->columna = columnaDeUsuario;
 	
 	}
   
-	int jugada::realizarJugada(){
+	int Jugada::realizarJugada(){
 		
 		int puntos=0;
 		
 		if(this->opcion=='m' || this->opcion=='M'){
 			
-			marcador(this->mapa*);
+			Marcador marcador(this->mapa*);
 			
 			puntos = marcador.marcar(this->fila , this ->columna);
 			
 		}else if(this->opcion == 'd' || this->opcion == 'D'){
 			
-			destapador(this->mapa , this->tablero);
+			Destapador destapador(this->mapa , this->tablero);
 			
 			puntos = destapador.destapar(this->fila , this->columna);
 		      
 		}else{
 			
-			puntos = SE_RETIRO
+			puntos = SE_RETIRO;
 		}
-		return puntos ;
+		return puntos;
 	}
