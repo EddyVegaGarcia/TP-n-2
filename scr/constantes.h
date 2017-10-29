83bc5cea9607c08086ca79bf402f13a67b6a646a
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-29 09:10:09 -0300

    Update constantes.h
    puede ganar :)

--- scr/constantes.h
+++ scr/constantes.h
@@ -6,9 +6,10 @@ const char MINA = 'X';
 const char BANDERA = 'B';
 
 const int PERDIO_PARTIDA = 3;
-const int NO_ESTA_JUGANDO = 5;
 const int SE_RETIRO = 4;
 const int SIGUE_JUGANDO = 0;
+const int HA_GANADO = 1;
+const int NO_ESTA_JUGANDO = 5;
 
 const uint FACIL=20;
 const uint MEDIO=30;
