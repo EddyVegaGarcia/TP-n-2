b91ed0da3d57fd6649db00688c8805cd23c07e66
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-29 06:37:58 -0300

    Update casilla.h

--- scr/casilla.h
+++ scr/casilla.h
@@ -8,24 +8,32 @@ private:
   
 public:
 
- /*Pre: recibe fila y columna dentro del rango indicado por el jugador
-  * Post: crea un objeto casilla, con la fila y columna recibidas. */
+ /* Pre: Recibe fila y columna dentro del rango indicado por el usuario.
+  * Post: Crea un objeto Casilla, con la fila y columna recibidas.*/
   Casilla(uint fila, uint columna);
   
-  /*Post: crea un objeto casilla, con fila y columna 0. */
+  /* Post: Crea un objeto Casilla, con fila y columna 0.*/
   Casilla();
   
-  /* Post: devuelve la fila*/
+  /* Post: Devuelve la fila de la Casilla.*/
   uint obtenerFila();
   
-  /* Post: devuelve la fila*/
+  /* Post: Devuelve la columna de la Casilla.*/
   uint obtenerColumna();
   
-  /* Post: devuelve el valor */
+  /* Post: Devuelve el valor de la Casilla.*/
   char obtenerValor();
-
+  
+ /* Pre: Recibe una fila dentro del rango válido.
+  * Post: Asigna la fila pasada como parámetro a la Casilla.*/
   void asignarFila(uint fila);
+
+ /* Pre: Recibe una columna dentro del rango válido.
+  * Post: Asigna la columna pasada como parámetro a la Casilla.*/
   void asignarColumna(uint columna);
+  
+ /* Pre: Recibe una variable "valor" de tipo char..
+  * Post: Asigna el valor pasado como parámetro a la Casilla.*/
   void asignarValor(char valor);
 
 };
