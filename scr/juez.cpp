345fa8e09b696d33cae3e185f26c49509b0ff4ce
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-29 09:25:51 -0300

    Update juez.cpp

--- scr/juez.cpp
+++ scr/juez.cpp
@@ -77,6 +77,7 @@ void Juez::inicializarJuego(){
 		jugadores->avanzarCursor();
 	}
 	
+	/* Queda un solo jugador */
 	avanzarJugador(); /* dejo el cursor apuntando al que sigue jugando */
 	while(jugadorActual->obtenerEstado() == SIGUE_JUGANDO){
 		if (casillasOcultas==minasPorDescubrir){
