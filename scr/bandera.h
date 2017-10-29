fb83b78dd2d65cdb82e2b11275f383500f7f6ae3
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-29 06:27:22 -0300

    Update bandera.h

--- scr/bandera.h
+++ scr/bandera.h
@@ -3,30 +3,32 @@
 
 typedef unsigned int uint;
 
-class bandera {
+class Bandera {
 
 private:
   uint fila;
   uint columna;
 
 public:
-/*Pre: recibe fila y columna dentro del rango indicado por el jugador
-* Post: crea un objeto bandera, con la fila y columna recibidas. */
+  /* Pre: recibe fila y columna dentro del rango indicado por el usuario.
+  * Post: crea un objeto Bandera, con la fila y columna recibidas.*/
   Bandera(uint fila, uint columna);
 
-  /*Post: crea un objeto bandera con fila y columna 0.*/
+  /* Post: crea un objeto bandera con fila y columna 0.*/
   Bandera();
 
-  /* Post: devuelve la fila*/
+  /* Post: devuelve la fila de la Bandera.*/
   uint obtenerFila();
 
-  /* Post: devuelve la fila*/
+  /* Post: devuelve la columna de la Bandera.*/
   uint obtenerColumna();
   
-  /* Post: asigna la fila pasada como parámetro a la bandera*/
+  /* Pre: recibe una fila dentro del rango válido.
+  * Post: asigna la fila pasada como parámetro a la bandera*/
   void asignarFila(uint fila);
   
-  /* Post: asigna la columna pasada como parámetro a la bandera*/
+  /* Pre: recibe una columna dentro del rango válido.
+  * Post: asigna la columna pasada como parámetro a la bandera.*/
   void asignarColumna(uint columna);
 };
 
