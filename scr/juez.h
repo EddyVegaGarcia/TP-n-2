c17957d613c0bbb471c0399e08ae123889c4acf4
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-29 09:52:23 -0300

    Update juez.h
    Quiéeeeeeen terminó parte del cpp y se quedó escribiendo todas las pre y postcondicioneeees B)))
    
    Regálenme comida (? :)

--- scr/juez.h
+++ scr/juez.h
@@ -3,6 +3,7 @@
 
 # include "mapa.h"
 # include "jugador.h"
+# include "constantes.h"
 # include <iostream>
 
 class Juez{
@@ -13,23 +14,50 @@ private:
 	Mapa* tableroDeJuego;
 
 public:
-
+	/* Pre: Recibe una "cantidadDeJugadores" de tipo int, y un puntero a Mapa o "tableroDeJuego".
+	* Post: Se crea un Juez con la cantidadDeJugadores y tableroDeJuego pasados por parámetros,
+	* 	deja creada una lista con cantidadDeJugadores punteros a Jugador.*/
 	Juez(unsigned int cantidadJugadores,Mapa* mapaRecibido);
 
-	void avanzarJugador();
-
-	void mostrarJugador();
-
-	void insertarJugador(Jugador* nuevo);
-	
+	/* Post: inicia el juego. Esto es, pide jugadas a los jugadores por turnos, las jugadas se realizan,
+	* 	se modifican los puntajes, se muestra el tablero, verifica si cada jugador ganó o perdió y se
+	* 	muestran los mensajes correspondientes. */
 	void inicializarJuego();
 	
-	void sigueJugando(Jugador* jugadorActual, int &jugadoresQuePerdieron, Mapa* tableroDeJuego);
-
-	void elJugadorHaPerdido(Jugador* jugadorActual);
-	
+	/* Post: Libera los recursos asociados a Juez.*/
 	~Juez();
 	
+private:
+	/* Pre: Se ha llamado a crearYAsignarListaDeJugadores()
+	* Post: Crea a los jugadores y los agrega a la lista.*/
+	void crearJugadores();
+	
+	/* Post: Crea una lista de jugadores.*/
+	void crearYAsignarListaDeJugadores();
+	
+	/* Pre: Se ha llamado a crearYAsignarListaDeJugadores(). Recibe un puntero a un Jugador.
+	* Post: Inserta "nuevo" a la lista de jugadores creada por crearYAsignarListaDeJugadores().*/
+	void insertarJugador(Jugador* nuevo);
+	
+	/* Post: avanza hasta el jugador más próximo que siga jugando.*/
+	void avanzarJugador();
+
+	void mostrarJugador(); /* A esto si lo quieren usar para algo, hay que cambiarlo (es corito, pero no sé qué quieren mostrar)*/
+
+	
+	/* Pre: Recibe un puntero al jugadorActual, una variable de tipo int que indica la cantidad de jugadores que perdieron,
+	* 	un puntero a Mapa, y una variable de tipo unsigned int que indica la cantidad de minas que quedan por descubrir
+	* 	en el tablero.
+	* Post: da su turno al jugadorActual y muestra el tablero de juego.*/
+	void Juez::sigueJugando(Jugador* jugadorActual, int &jugadoresQuePerdieron, Mapa* tableroDeJuego, uint &minasPorDescubrir);
+
+	/* Pre: Recibe "jugadorActual", puntero a Jugador que perdió.
+	* Post: Muestra un mensaje a "jugadorActual" con su puntaje.*/
+	void mostrarPuntajeDeJugadorQueHaPerdido(Jugador* jugadorActual);
+	
+	/* Pre: Recibe "jugadorActual", puntero a Jugador que ganó.
+	* Post: Muestra un mensaje de felicitaciones a "jugadorActual" con su puntaje.*/
+	void mostrarFelicitaciones(Jugador* jugadorActual);
 
 
 };
