d5b8086ff831fa36f52fc9da8be09448e2302eed
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-29 06:57:19 -0300

    Update constantes.h

--- scr/constantes.h
+++ scr/constantes.h
@@ -1,7 +1,6 @@
 
 typedef unsigned int uint;
 
-
 const char VACIO = '0';
 const char MINA = 'X';
 const char BANDERA = 'B';
@@ -10,9 +9,10 @@ const int PERDIO_PARTIDA = 3;
 const int NO_ESTA_JUGANDO = 5;
 const int SE_RETIRO = 4;
 const int SIGUE_JUGANDO = 0;
-const unsigned int facil=20;
-const unsigned int medio=30;
-const unsigned int dificil=40;
+
+const uint FACIL=20;
+const uint MEDIO=30;
+const uint DIFICIL=40;
 
 // pongan sus constanteessssss :)
 
