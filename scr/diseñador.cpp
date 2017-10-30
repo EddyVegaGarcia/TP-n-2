f79b7ce39c073fc93ed170528dd8c9f5352e1538
Tubio <32169593+Tubio@users.noreply.github.com>
2017-10-30 12:05:25 -0300

    Update diseñador.cpp

--- scr/diseñador.cpp
+++ scr/diseñador.cpp
@@ -1,38 +1,100 @@
-#ifndef DISENIADOR_H_
-#define DISENIADOR_H_
-
-#include "EasyBMP_BMP.h"
-#include "EasyBMP_DataStructures.h"
-#include "EasyBMP_VariousBMPutilities.h"
+#include "diseniador.h"
 
 
-class Diseniador{
-
-private:
+Diseniador::Diseniador(BMP baseMapa, unsigned int filas, unsigned int columnas){
 	
-	BMP aDiseniar;
-	unsigned int cantidadFilas;
-	unsigned int cantidadColumnas;
+	this->aDiseniar = baseMapa;
+	
+this->llenarMapaBloqueado(baseMapa);
+this->llenarEsquinas(baseMapa);
+this->llenarBordes(baseMapa);
 
-void llenarMapaBloqueado();
-void llenarEsquinas();
-void llenarBordes();
+}
 
-void llenarBordeSuperior();
-void llenarBordeInferior();
-void llenarBordeIzquierdo();
-void llenarBordeDerecho();
+void Diseniador::llenarEsquinas(){
 
-void llenarEsquinaSuperiorDerecha();
-void llenarEsquinaSuperiorIzquierda();
-void llenarEsquinaInferiorDerecha();
-void llenarEsquinaInferiorIzquierda();
+	llenarEsquinaSuperiorDerecha();
+	llenarEsquinaSuperiorIzquierda();
+	llenarEsquinaInferiorDerecha();
+	llenarEsquinaInferiorIzquierda();
 
-public:
+}
+void llenarEsquinaSuperiorDerecha(){}
+void llenarEsquinaSuperiorIzquierda(){}
+void llenarEsquinaInferiorDerecha(){}
+void llenarEsquinaInferiorIzquierda(){}
+
+void Diseniador::llenarBordes(){
+
+	llenarBordeSuperior();
+	llenarBordeInferior();
+	llenarBordeIzquierdo();
+	llenarBordeDerecho();
+
+}
+void llenarBordeSuperior(){
+
+	for(int i= 0; (i<this->cantColumnas)*2; i++){
+
+			RangedPixelToPixelCopy(##,1,16,16,1,##baseMapa,
+					1+BORDES+(i*BORDES),1+BORDES+(this->cantFilas*CELDAS));
+		}
+
+}
+void llenarBordeInferior(){
+
+	for(int i= 0; (i<this->cantColumnas)*2; i++){
+
+			RangedPixelToPixelCopy(##,1,16,16,1,##baseMapa,
+					1+BORDES+(i*BORDES),1+BORDES);
+		}
+}
+void llenarBordeIzquierdo(){
+
+	for(int i= 0; i<this->cantFilas; i++){
+
+		RangedPixelToPixelCopy(##,1,16,16,1,##baseMapa,
+				1,1+BORDES+(i*BORDES));
+	}
+
+}
+void llenarBordeDerecho(){
+
+	for(int i= 0; i<this->cantFilas; i++){
+
+			RangedPixelToPixelCopy(##,1,16,16,1,##baseMapa,
+					1+BORDES+(this->dimColumna*CELDAS),1+BORDES+(i*BRODES));
+		}
+}
+
+void Diseniador::llenarMapaBloqueado(){
+
+	for(int i=0; i<this->dimFila; i++){
+
+		for(int j=0; j<this->dimColumna; i++){
+
+			RangedPixelToPixelCopy(ARcasillasBloqueadas,1,16,16,1,basemapa,
+					(BORDES+1)+(i*CASILLAS),(BORDES+1)+(i+CASILLAS))
+		}
+	}
 
 
-Diseniador(BMP baseMapa,unsigned int filas,unsigned int columnas);
+}
 
-};
-
-#endif /* DISENIADOR_H_ */
+void llenarEsquinaSuperiorDerecha(){
+	
+	RangedPixelToPixelCopy(ARcasillasBloqueadas,1,16,16,1,basemapa,
+						(BORDES+1)+(i*CASILLAS),(BORDES+1)+(i+CASILLAS))
+}
+void llenarEsquinaSuperiorIzquierda(){
+	
+	
+}
+void llenarEsquinaInferiorDerecha(){
+	
+	
+}
+void llenarEsquinaInferiorIzquierda(){
+	
+	
+}
