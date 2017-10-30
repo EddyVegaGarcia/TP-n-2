09d9abeb29b4c6a3ef4bc92d091ce39430fdfb49
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-10-30 19:40:16 -0300

    Update constantes.h

--- scr/constantes.h
+++ scr/constantes.h
@@ -1,9 +1,12 @@
+#ifndef CONSTANTES_H_
+#define CONSTANTES_H_
 
 typedef unsigned int uint;
 
 const char VACIO = '0';
 const char MINA = 'X';
 const char BANDERA = 'B';
+
 const char NADA = 'N';
 
 const int PERDIO_PARTIDA = 3;
@@ -16,6 +19,20 @@ const uint FACIL=20;
 const uint MEDIO=30;
 const uint DIFICIL=40;
 
+const uint CELDAS=16;
+const uint BORDES=8;
+											//iniciales=
+const char archivoN[] = "topblue.bmp"; 		//norte
+const char archivoS[] = "bottomblue.bmp"; 	//sur
+const char archivoE[] = "rightblue.bmp"; 	//este
+const char archivoO[] = "leftblue.bmp"; 	//oeste
+
+const char archivoNO[] = "topleftblue.bmp";	//noroeste
+const char archivoNE[] = "toprightblue.bmp";//noreste
+const char archivoSO[] = "bottomleft.bmp";	//suroeste
+const char archivoSE[] = "bottomright.bmp";	//sureste
+
+const char bloqueado[] = "blockedblue.bmp";
 const char archivoMina[] = "mineblue.bmp" ;
 const char archivoBandera[] = "flaggedblue.bmp" ;
 const char archivoVacio[] = "emptyblue.bmp" ;
@@ -27,8 +44,10 @@ const char archivo5[] = "fiveblue.bmp" ;
 const char archivo6[] = "sixblue.bmp" ;
 const char archivo7[] = "sevenblue.bmp" ;
 const char archivo8[] = "eightblue.bmp" ;
+const char archivoDeJuego[] = "currentGame.bmp";
 
 const char archivoDePuntajes[] = "puntajes.txt";
 
 // pongan sus constanteessssss :)
 
+#endif /*CONSTANTES_H_*/
