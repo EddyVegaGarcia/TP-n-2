059590403a33981c9436056f3e7bb28024422bfb
EddyVegaGarcia <32282833+EddyVegaGarcia@users.noreply.github.com>
2017-10-30 15:57:59 -0300

    Update constantes.h

--- scr/constantes.h
+++ scr/constantes.h
@@ -4,6 +4,7 @@ typedef unsigned int uint;
 const char VACIO = '0';
 const char MINA = 'X';
 const char BANDERA = 'B';
+const char NADA = 'N';
 
 const int PERDIO_PARTIDA = 3;
 const int SE_RETIRO = 4;
