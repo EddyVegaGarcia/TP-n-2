b89e189cf69ad367a2cbda1af00a4191278b6862
dafi <32344462+dafifynn@users.noreply.github.com>
2017-10-30 00:35:54 -0300

    Create diseñador.h

---
+++ scr/diseñador.h
@@ -0,0 +1,13 @@
+class diseñador{
+
+private:
+
+void Diseñador::llenarMapaBloqueado(BMP baseMapa);
+void Diseñador::llenarEsquinas(BMP baseMapa);
+void Diseñador::llenarBordes(BMP baseMapa);
+
+public:
+
+
+BMP Diseñador::diseña(BMP baseMapa);
+};
