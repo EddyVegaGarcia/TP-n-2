1b4e80ddeba427a29499fb97af5187d210fc148f
dafi <32344462+dafifynn@users.noreply.github.com>
2017-11-20 18:50:10 -0300

    Update jugada.cpp

--- src2/jugada.cpp
+++ src2/jugada.cpp
@@ -35,15 +35,18 @@ using namespace std;
 		
 		if(this->opcion=='m' || this->opcion=='M'){
 			
-			Marcador marcador(this->mapa);
+			Marcador* marcador = new Marcador(this->mapa);
 			
-			puntos = marcador.marcar(this->fila, this ->columna);
+			puntos = marcador->marcar(this->fila, this ->columna);
 			
+			delete marcador;
 		}else if(this->opcion == 'd' || this->opcion == 'D'){
 			
-			Destapador destapador(this->mapa);
+			Destapador* destapador = new Destapador(this->mapa);
 			
-			puntos = destapador.destapar(this->fila , this->columna);
+			puntos = destapador->destapar(this->fila , this->columna);
+
+			delete destapador;
 		      
 		}else{
 			puntos = SE_RETIRO;
