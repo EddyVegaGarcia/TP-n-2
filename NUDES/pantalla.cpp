b72286e998f216281d9b3e6799c6db1fec3b34f3
dafi <32344462+dafifynn@users.noreply.github.com>
2017-12-09 18:27:50 -0300

    Update pantalla.cpp

--- NUDES/pantalla.cpp
+++ NUDES/pantalla.cpp
@@ -4,6 +4,11 @@
  *  Created on: Dec 9, 2017
  *      Author: marcela
  */
+
+void playGame(){
+    std::cout<<".:PLAY GAME::."<<std::endl;
+
+}
 char pedirOpcionPasadoFuturo (){
     
    char opcionDeUsuario;
@@ -16,3 +21,39 @@ char pedirOpcionPasadoFuturo (){
     
     return opcionDeUsuario;
  }
+
+int pedirOpcionRehacerJugada (PseudoGrafo<JugadaLight*>* jugadas ){
+uint contador = 1;
+	uint opcionUsuario;
+	bool puedeRehacerJugada = true;
+
+	JugadaLight* actual;
+        do {
+	std::cout<<"jugadas realizadas desde este punto: "<<std::endl;
+
+	this->jugadas->avanzarCursor();
+	actual = this->jugadas->obtenerDatoActual();
+	if(actual == NULL){
+		puedeRehacerJugada = false;
+	}
+
+
+	std::cout<<"jugada 1: ";
+	std::cout<<"fila: "<<actual->obtenerFila()<<" columna: "<<actual->obtenerColumna()
+			<<" opcion: "<<actual->obtenerOpcion()<<std::endl;
+
+	while(this->jugadas->avanzarCursorAParalela()){
+		contador++;
+		actual=this->jugadas->obtenerDatoActual();
+		std::cout<<"jugada "<<contador<<": ";
+		std::cout<<"fila: "<<actual->obtenerFila()<<" columna: "<<actual->obtenerColumna()
+			<<" opcion: "<<actual->obtenerOpcion()<<std::endl;
+	}
+	this->jugadas->retrocederCursor();
+
+	std::cout<<std::endl<<"elija una de las jugadas para rehacer"<<std::endl;
+	std::cin>>opcionUsuario;
+		
+        }while (opcionUsuario < 1||opcionUsuario );
+    return opcionUsuario;
+}
