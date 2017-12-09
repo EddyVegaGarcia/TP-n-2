92d84219f534e16ca17f5db6ede3f41fbffd34ca
dafi <32344462+dafifynn@users.noreply.github.com>
2017-12-09 18:15:44 -0300

    Update pantalla.cpp

--- NUDES/pantalla.cpp
+++ NUDES/pantalla.cpp
@@ -4,7 +4,15 @@
  *  Created on: Dec 9, 2017
  *      Author: marcela
  */
+char pedirOpcionPasadoFuturo (){
+    
+   char opcionDeUsuario;
 
+    std::cout<<"VOLVER AL PASADO 'deshacer' (P) || ";
+		std::cout<<"VOLVER AL FUTURO 'rehacer' (F) || "<<std::endl;
+		std::cout<<"Ya no realizar JUGADA ESPECIAL (N) : "<<std::endl;
 
-
-
+		std::cin>>opcionDeUsuario;
+    
+    return opcionDeUsuario;
+ }
