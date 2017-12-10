43ee1371184e99918aca76ab504188bdceff354d
Aldy09 <32624398+Aldy09@users.noreply.github.com>
2017-12-10 00:26:43 -0300

    ayuda, qué hace el constructor? atributos??

--- NUDES/pantalla.h
+++ NUDES/pantalla.h
@@ -1,14 +1,47 @@
-/*
- * pantalla.h
- *
- *  Created on: Dec 9, 2017
- *      Author: marcela
- */
 #ifndef PANTALLA_H_
 #define PANTALLA_H_
 
-void noHayJugadas();
+# include "pseudoGrafo.h"
+# include "jugadaLight.h"
+# include "constantes.h"
+# include <iostream>
 
+class Pantalla{
+  
+public:  
+
+  void playGame();
+
+  char pedirOpcionPasadoFuturo();
+
+  int pedirOpcionRehacerJugada (PseudoGrafo<JugadaLight*>* jugadas);
+
+  void noSePuedeRetroceder();
+
+  void mostrarPuntajeDeJugadorQueHaPerdido(Jugador* jugadorActual);
+
+  void terminoLaPartida();
+
+  void noHayJugadas();
+
+
+  char imprimirOpcionesDeMenu();
+
+  void imprimirReglasDeJuego();
+
+  void imprimirLinea();
+
+  void imprimirTitulo();
+
+  void imprimirPidiendoDatos();
+
+  void imprimirDatosIngresados();
+
+  char pedirDificultad();
+
+  uint pedirCantidadJugadores();
+  
+};
 
 #endif
 
